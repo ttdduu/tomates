b77d1109f4907c997b5243e0fae5602326d70ed4
tdu <tommy.deudaeta@gmail.com>
2023-02-06 15:36:10 -0300

    bla

--- tdubot.py
+++ tdubot.py
@@ -129,7 +129,7 @@ def apagar(m):
         return
     else:
         os.system('cd /home/tdu/code/tomates && git pull origin')
-        os.system('pid=$(ps -ef | grep python | awk "{print $2}" | head -n 1) && echo $pid')
+        os.system('pid=$(ps -ef | grep tdubot | awk "{print $2}" | head -n 1 | awk "{match($0,/[0-9]+/); print substr($0,RSTART,RLENGTH)}") && echo $pid')
 # }}}
 # {{{tomate
 def cargar():
