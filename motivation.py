f7bbf2b177acf63f7c35bc3acf49fcac7986fc6e
ttdduu <tommy.deudaeta@gmail.com>
2023-08-28 16:16:28 -0300

    kjsdj

--- motivation.py
+++ motivation.py
@@ -1,18 +1,3 @@
-#!/usr/bin/env python3
+#!/usr/bin/zsh
 
-import os
-import time
-
-# segs = 5
-# t=segs
-# while t>1:
-    # time.sleep(1)
-    # os.system(f'echo "lo que haga día a día determina el Yo que voy a ser en un año"')
-    # t-=1
-
-# os.system(f'echo "lo que haga día a día determina el Yo que voy a ser en un año"')
-# os.system(f'echo "deprimí los circuitos que te hacen perder, potenciá los que necesitás."')
-frases=['deprimí los circuitos que toman control de tus conductas y te llevan a hacer lo que no querés, potenciá los que necesitás para hacer lo que querés.','pensá en toda la gente en el mundo que está concentrada estudiando un tema. Entre ellos, pensá en todos los que tienen buenas intenciones. Sumergite en concentración junto a ellos.']
-# time.sleep(5)
-#
-
+echo 'luck favours the prepared, pero descansá 5 minutos'
